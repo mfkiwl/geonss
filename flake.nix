2a5e2b96cd49a755ce5ffa49ec8068f3ee8c1799
Parrot7483 <>
2025-09-06 12:27:26 +0200

    simplify nix flake

--- flake.nix
+++ flake.nix
@@ -3,62 +3,66 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     requirements = {
       url = "path:./requirements.txt"; # Track requirements.txt as an input
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, requirements }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        python3 = pkgs.python3;
+  outputs = { self, nixpkgs, requirements }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          python3 = pkgs.python3;
 
-        # Required libraries for compiled Python packages (like NumPy)
-        pythonLDLibPath = pkgs.lib.makeLibraryPath (with pkgs; [
-          stdenv.cc.cc
-          glibc
-          zlib
-          openblas
-        ]);
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            python3
-            python3Packages.virtualenv
+          # Required libraries for compiled Python packages (like NumPy)
+          pythonLDLibPath = pkgs.lib.makeLibraryPath (with pkgs; [
+            glibc
+            openblas
+            stdenv.cc.cc
+            zlib
+          ]);
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              python3
+              python3Packages.virtualenv
 
-            # Development
-            python3Packages.jupyterlab
-            python3Packages.ipython
+              # Development
+              python3Packages.jupyterlab
+              python3Packages.ipython
 
-            # Added for binary compatibility
-            autoPatchelfHook
-          ];
+              # Added for binary compatibility
+              autoPatchelfHook
+            ];
 
-          shellHook = ''
-              export SHELL=${pkgs.bashInteractive}/bin/bash
-              export VENV_DIR=".venv"
-              export PIP_DISABLE_PIP_VERSION_CHECK=1
+            shellHook = ''
+                export SHELL=${pkgs.bashInteractive}/bin/bash
+                export VENV_DIR=".venv"
+                export PIP_DISABLE_PIP_VERSION_CHECK=1
+                export LD_LIBRARY_PATH="${pythonLDLibPath}"
 
-              export LD_LIBRARY_PATH="${pythonLDLibPath}"
+                if [ ! -d "$VENV_DIR" ]; then
+                  echo "Creating virtual environment in $VENV_DIR..."
+                  ${python3}/bin/python -m venv $VENV_DIR
+                fi
 
-              if [ ! -d "$VENV_DIR" ]; then
-                echo "Creating virtual environment in $VENV_DIR..."
-                ${python3}/bin/python -m venv $VENV_DIR
-              fi
+                # Activate the virtual environment
+                source "$VENV_DIR/bin/activate"
 
-              # Activate the virtual environment
-              source "$VENV_DIR/bin/activate"
+                echo "Installing and upgrading dependencies..."
+                pip install --upgrade -r ${requirements}
+                pip install -e .
 
-              echo "Installing and upgrading dependencies..."
-              pip install --disable-pip-version-check --upgrade -r ${requirements}
-              pip install --disable-pip-version-check -e .
-
-              autoPatchelf
-          '';
-        };
-      }
-    );
+                autoPatchelf
+            '';
+          };
+        }
+      );
+    };
 }
